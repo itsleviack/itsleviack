fe301d77639a7ef0693a5eb2b4547c500e3dd72e
Nick <fnick1703@gmail.com>
2021-12-23 00:17:35 +0300

    main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	int hour1, hour2, minute1, minute2, result = 0;
+	scanf_s("%d", &hour1); scanf_s("%d", &minute1);
+	scanf_s("%d", &hour2); scanf_s("%d", &minute2);
+	while ((hour1 != hour2) || (minute1 != minute2))
+	{
+		if (minute1 == 30)
+			result+= 1;
+		minute1++;
+		if (minute1 == 60)
+		{
+			minute1 = 0;
+			hour1 += 1;
+			if (hour1 >> 12)
+				result += hour1 - 12;
+			else
+				result += hour1;
+			if (hour1 == 24)
+				hour1 = 0;
+		}
+		printf("%d", result);
+		return 0;
+	}
+}
