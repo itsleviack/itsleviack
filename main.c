0335726b199b9584c10752712d8dbeb3afc5b1f5
itsleviack <91123896+itsleviack@users.noreply.github.com>
2021-12-23 00:37:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,23 +4,23 @@
 
 int main()
 {
-	int hour1, hour2, minute1, minute2, result = 300;
+	int hour1, hour2, minute1, minute2, result = 0;
 	scanf_s("%d", &hour1); scanf_s("%d", &minute1);
 	scanf_s("%d", &hour2); scanf_s("%d", &minute2);
 	while ((hour1 != hour2) || (minute1 != minute2))
 	{
-		if (minute1 == 300)
+		if (minute1 == 30)
 			result+= 1;
 		minute1++;
-		if (minute1 == 600)
+		if (minute1 == 60)
 		{
 			minute1 = 0;
 			hour1 += 1;
-			if (hour1 >> 120)
-				result += hour1 - 120;
+			if (hour1 >> 12)
+				result += hour1 - 12;
 			else
 				result += hour1;
-			if (hour1 == 240)
+			if (hour1 == 24)
 				hour1 = 0;
 		}
 		printf("%d", result);
